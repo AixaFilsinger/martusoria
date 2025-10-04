a0281e4416bdc52a2e437cbbd418870d0a8987c0
Aixa Filsinger <macafil99@gmail.com>
2025-10-04 17:01:15 -0300

    frase

--- src/views/Frase.jsx
+++ src/views/Frase.jsx
@@ -1,7 +1,7 @@
 import destello from '../assets/descarga (1).jpg'
 const Frase = () => {
     return (
-        <section className=' d-flex flex-colum frase'>
+        <section className=' d-flex flex-colum frase pt-3'>
             <img src={destello} alt="" width={100}/>
             <p className='parrafo'>Existen momentos en la vida que imaginamos, soñamos y esperamos.
                 <br/>Uno de esos momentos es este.</p>
